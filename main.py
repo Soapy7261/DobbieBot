2551dc0d99cb1991090c08ff8e50151ad29f0edb
Dobby-progam <dthweathertime@gmail.com>
2022-08-23 18:43:45 +0200

    updated main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
+intents.message_content = True
 bot =commands.Bot(debug_guilds=[955135608228024394], command_prefix="-", status=discord.Status.dnd,activity=discord.Activity(type=discord.ActivityType.listening, name="keyboard noises"),owner='820255805257023498',intents=intents,)
 
 
 @bot.listen()
 async def on_ready():
     print("Now ready!")
-    embed = discord.Embed(title="Online! \nTime to mess around!", color=0x00ff00,)
+    embed = discord.Embed(title=":green_circle: Online!  \nTime to mess around!", timestamp=discord.utils.utcnow(), color=0x00ff00,)
     await bot.get_guild(955135608228024394).get_channel(1011649871511572500).send(embed=embed)
 
 
@@ -17,6 +18,11 @@ async def on_ready():
 async def hello(ctx):
     await ctx.reply("Hello!")
     print("send command 'hello'.")
+
+@bot.command()
+async def hello_hi(ctx):
+   embed= discord.Embed(title="embed works", timestamp=discord.utils.utcnow(), color=0x00ff00,)
+   await ctx.send(embed=embed)
 
 
 @bot.command()
@@ -45,6 +51,13 @@ async def divide(ctx, divide1: int, divide2: int):
     await ctx.send(f"{divide1} / {divide2} = {divide1 / divide2}")
     print("command used 'divide'.")
     print(f"{divide1} / {divide2} = {divide1 / divide2}")
+
+
+@bot.command()
+async def sub(ctx, subt1: int, subt2: int):
+    await ctx.send(f"{subt1}-{subt2} = {subt1-subt2}")
+    print("command sub is used"+f"{subt1}-{subt2} = {subt1-subt2}")
+
 
 
 @bot.command()
