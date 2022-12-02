8341d2e315ff68d853b7da1533db6a929095f6a3
Soapy7261 <81933267+Soapy7261@users.noreply.github.com>
2022-12-02 12:59:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ async def on_command_error(ctx, error):
 
         await bot.get_guild(955135608228024394).get_channel(1017880577506017361).send(embed=embed)
 
-@bot.event
+@bot.listen()
 async def on_message(message):
     if "dobbie" in message.content.lower():
         await message.add_reaction("👀")
