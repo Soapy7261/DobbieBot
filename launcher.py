f2c596de1c86edfef8d6d10c95b4b3fcc0564336
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2022-12-04 20:59:41 -0500

    I hate pylint

--- launcher.py
+++ launcher.py
@@ -4,10 +4,10 @@ while True:
     print ("Starting bot...")
     g = subprocess.run("git pull", check=True) #Pulls the latest code from GitHub
     if g.returncode != 0:
-        print ("""Error pulling code from github, this is 
-            likely because you don't have git installed, 
-            if you do have git installed, 
-            please check your configuration for git 
+        print ("""Error pulling code from github, this is
+            likely because you don't have git installed,
+            if you do have git installed,
+            please check your configuration for git
             and try again""")
     p = subprocess.run("py main.py", check=True)
     if p.returncode != 0:
